eacc65abd0306f3109e89ee9356c809f9ba53abd
Weikun Liang <weikunl@andrew.cmu.edu>
2015-11-18 18:51:46 -0500

    local storage

--- popup.js
+++ popup.js
@@ -1,3 +1,23 @@
+function save() {
+  var txt = $("#saveTxt").text();
+  if(!txt) {
+    console.log("Error!");
+  } else {
+    chrome.storage.local.set({'user': txt}, function() {
+      console.log("Saved!");
+    });
+  }
+}
+
+function load() {
+  var user = "";
+  chrome.storage.local.get('user', function (result) {
+        user = result.channels;
+        console.log(result.channels);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   // do something
+  save();
 });
