42135ef6a9bf0b74b55d7b155370708356e4437e
Weikun Liang <weikunl@andrew.cmu.edu>
2015-12-03 00:21:58 -0500

    added static methods

--- contact.js
+++ contact.js
@@ -1,26 +1,38 @@
+var contactList = {};
+
 function Contact(contact_id, contact_pub_key) { 	
 	this.contact_id = contact_id;
 	this.contact_pub_key = contact_pub_key;	
 	this.sessions = {};
 }	
 
-Contact.prototype.addSession = function(contact_id, session_id, sessionInfo) {
-	contact_id = contact_id || this.contact_id;
+Contact.prototype.addSession = function(session_id, sessionInfo) {
 	this.sessions[session_id] = sessionInfo;
 }		
 
-Contact.prototype.getSessionId = function(contact_id, encrypted_message) {
-	contact_id = contact_id || this.contact_id;
+Contact.addSession = function(contact_id, session_id, sessionInfo) {
+	var c = contactList[contact_id];
+	c.sessions[session_id] = sessionInfo;
+}
+
+Contact.prototype.getSessionId = function(encrypted_message) {
 	// I don't know how you want to get the session ID from the encrypted message. Don't you also need which session it is? 
 	// Shouldn't it be like a user(contact) each have an array of contacts where they store information like keys, sessions, etc.
 	return "sessionID";
 }	
 
-Contact.prototype.decrypt = function(contact_id, current_session_id, encrypted_message) {
-	contact_id = contact_id || this.contact_id;
+Contact.getSessionId = function(contact_id, encrypted_message) {
+	return;
+}
+
+Contact.prototype.decrypt = function(current_session_id, encrypted_message) {
 	var session_id = getSessionId(contact_id, encrypted_message);
 	// Do fancy decryption which I also don't know how you do.
 	return "fancy decryption";
+}
+
+Contact.decrypt = function(contact_id, current_session_id, encrypted_message) {
+	return;
 }
 
 /*
@@ -28,5 +40,3 @@ Contact.prototype.decrypt = function(contact_id, current_session_id, encrypted_m
  */
  
 module.exports = Contact;
-
-
